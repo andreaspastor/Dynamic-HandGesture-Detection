20f84602755030195ede0905fd50e1378ffb33d2
Andréas PASTOR <andreas.pastor@etu.univ-nantes.fr>
2017-12-09 06:58:05 +0100

    Add threads dropout and npy

--- dataPickle.py
+++ dataPickle.py
@@ -1,16 +1,49 @@
 import glob
 import numpy as np 
-import pickle
 import cv2
 import random
 import os
+import sys
+from PIL import Image
+from threading import Thread, RLock
+from time import time
 
+rlock = RLock()
+
+class OpenImage(Thread):
+	""" Thread to open images. """
+	def __init__(self, listA):
+	    global data, cptOccur, idmove
+	    Thread.__init__(self)
+	    self.listA = listA
+	    self.img, self.value, self.imgs = None, None, None
+
+	def run(self):
+		""" Code to execute to open. """
+		for elm in self.listA:
+			if idmove[elm[1]] in [0,1,2,3,4]:
+				self.img = sorted(glob.glob('./20bn-jester-v1/' + elm[0] + '/**'))
+				self.imgs = []
+				if len(self.img[::8]) == 5:
+					cptOccur[idmove[elm[1]]] += 1
+					for image in self.img[::8]:
+						self.imgs.append(np.array(cv2.resize(cv2.imread(image, 0), (imgSize,imgSize))))
+					self.stacked = [np.dstack(self.imgs),idmove[elm[1]]]		        
+					with rlock:
+						data.append(self.stacked)
+					for x in [-5,5]:
+						self.imgs = []
+						for image in self.img[::8]:
+							self.imgs.append(np.array(Image.fromarray(cv2.resize(cv2.imread(image, 0), (imgSize,imgSize))).rotate(x)))
+						self.stacked = [np.dstack(self.imgs),idmove[elm[1]]]		        
+						with rlock:
+							data.append(self.stacked)
 
 idmove = {'Swiping Left' : 0,
 		  'Swiping Right' : 1,
 		  'Swiping Down' : 2,
 		  'Swiping Up' : 3,
-		  'Pushing Hand Away' : 4,
+		  'Pushing Hand Away' : 26,
 		  'Pulling Hand In' : 5,
 		  'Sliding Two Fingers Left' : 6,
 		  'Sliding Two Fingers Right' : 7,
@@ -32,12 +65,11 @@ idmove = {'Swiping Left' : 0,
 		  'Stop Sign' : 23,
 		  'Drumming Fingers' : 24,
 		  'No gesture' : 25,
-		  'Doing other things' : 26
+		  'Doing other things' : 4
 		}
 
-
-nbClass = len(idmove)
-cptOccur = [0 for x in range(nbClass)]
+nbClass = 5#len(idmove)
+cptOccur = [0 for x in range(len(idmove))]
 split = 0.9
 imgSize = 64
 liste = glob.glob("./20bn-jester-v1/**")
@@ -53,51 +85,69 @@ while ligne != "":
 
 file.close()
 
-random.shuffle(liste)
-print(len(liste))
-cpt = 0
+random.shuffle(moves)
+print(len(moves))
+#moves = moves[:100000]
+print(len(moves))
 data = []
-for elm in moves[:]:
-	images = sorted(glob.glob('./20bn-jester-v1/' + elm[0] + '/**'))
-	imgs = []
-	if cpt % 1000 == 0:
-		print(cpt,'/',20000)
-	if cpt > 20000:
-		break
-	if len(images[::4]) == 10:
-		cptOccur[idmove[elm[1]]] += 1
-		cpt += 1
-		for image in images[::4]:
-			imgs.append(np.array(cv2.resize(cv2.imread(image, 0), (imgSize,imgSize))))
-			"""cv2.imshow('frame', imgs[-1])
-			if cv2.waitKey(100) & 0xFF == ord('q'):
-				cv2.destroyAllWindows()
-				break"""
-		data.append([np.dstack(imgs),idmove[elm[1]]])
+
+#Chargement en RAM des images trouvees
+# Threads Creation
+t1 = time()
+threads = []
+
+nbThread = 20
+size = int(len(moves)/nbThread)
+for x in range(nbThread):
+    threads.append(OpenImage(moves[x*size:(x+1)*size]))
+
+# Lancement des threads
+for thread in threads:
+    thread.start()
+
+
+# Attend que les threads se terminent
+for thread in threads:
+    thread.join()
+
 
 print(len(data))
 
+random.shuffle(data)
 print('Chargement en RAM des images done ...')
-#Traitement des images pour l'entrainement du modèle
+#Traitement des images pour l'entrainement du modele
 X_train = []
 y_train = []
 data_train = []
 for elm in data[:int(len(data)*split)]:
-  classe = np.zeros(nbClass)
-  classe[elm[1]] = 1
-  data_train.append([np.flip(elm[0],1), classe])
-  data_train.append([elm[0], classe])
+	classe = np.zeros(nbClass)
+	classe[elm[1]] = 1
+	data_train.append([elm[0], classe])
+	if elm[1] == 0:
+		classe = np.zeros(nbClass)
+		classe[1] = 1
+	elif elm[1] == 1:
+		classe = np.zeros(nbClass)
+		classe[0] = 1
+	data_train.append([np.flip(elm[0],1), classe])
+  
 
 print('Traitement data_train done ...')
-#Traitement des images pour le test du modèle
+#Traitement des images pour le test du modele
 X_test = []
 y_test = []
 data_test = []
 for elm in data[int(len(data)*split):]:
-  classe = np.zeros(nbClass)
-  classe[elm[1]] = 1
-  data_test.append([np.flip(elm[0],1), classe])
-  data_test.append([elm[0], classe])
+	classe = np.zeros(nbClass)
+	classe[elm[1]] = 1
+	data_test.append([elm[0], classe])
+	if elm[1] == 0:
+		classe = np.zeros(nbClass)
+		classe[1] = 1
+	elif elm[1] == 1:
+		classe = np.zeros(nbClass)
+		classe[0] = 1
+	data_test.append([np.flip(elm[0],1), classe])
 
 print('Traitement data_test done ...')
 data = 0
@@ -133,13 +183,13 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
 
-pickle.dump(X_train, open('./dataTrain/Xtrain.dump', 'wb'))
-pickle.dump(y_train, open('./dataTrain/Ytrain.dump', 'wb'))
-pickle.dump(X_test, open('./dataTrain/Xtest.dump', 'wb'))
-pickle.dump(y_test, open('./dataTrain/Ytest.dump', 'wb'))
+np.save('./dataTrain/Xtrain', X_train)
+np.save('./dataTrain/Ytrain', y_train)
+np.save('./dataTrain/Xtest', X_test)
+np.save('./dataTrain/Ytest', y_test)
 
-pickle.dump(XClassTest, open('./dataTrain/XtestClass.dump', 'wb'))
-pickle.dump(YClassTest, open('./dataTrain/YtestClass.dump', 'wb'))
+np.save('./dataTrain/XtestClass', XClassTest)
+np.save('./dataTrain/YtestClass', YClassTest)
 
 
 print("Nombres exemples d'entrainement", len(X_train))
